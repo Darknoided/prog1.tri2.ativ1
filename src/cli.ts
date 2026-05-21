0ea30147241f04cf8c56cd7bb1380e9fd533c393
Your Name <you@example.com>
2026-05-21 11:54:59 -0300

    criado a feature do cli para adicionar itens

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,31 @@
 //CLI: Command Line Interface
 
+import TodoListApelido, { Item } from './core'
+
+const TodoList = new TodoListApelido('todoList.json')
+const params = process.argv;
+const command = params[2];
+
+if (command === 'add'){
+    const value = params[3];
+
+    if (!value) {
+        console.log('Valor do item não pode ser nulo ou vazio')
+        process.exit(1)
+    }
+
+    try {
+        await TodoList.addItem(new Item(value))
+    } catch (error) {
+        console.error('Erro ao adicionar item.', error)
+        process.exit(1)
+    }
+    
+    console.log('Item adicionado com sucesso:', value)
+    process.exit(0);
+}
+
+if (command)
+    console.log('Comando não reconhecido: ${command}')
+
+console.log('não entrou em nenhum if');
--- src/core.ts
+++ src/core.ts
@@ -1,24 +1,57 @@
 class Item {
-    constructor(public title: string) {}
+    constructor(public title: string){}
 }
 
 class TodoList {
     private items: Item[] = [];
     private filePath: string;
-    constructor(filePath: string) {
+
+    constructor(filePath: string){
         this.filePath = filePath;
+        this.readListFromDisk();
     }
 
-    addItem(item: Item) {
+    private async saveListToDisk(){
+        const file = Bun.file(this.filePath);
+        const data = JSON.stringify(this.items);
+        await file.write(data);
+    }
+
+    private async readListFromDisk(){
+        const file = Bun.file(this.filePath);
+        // const text = await file.text();
+        // const data = JSON.parse(text);
+        const data = await file.json();
+        this.items = data.map((v: any) => {
+            return new Item(v.title);
+        })
+    }
+
+    /**
+     * Adiciona um item a lista
+     */
+    async addItem(item: Item){
+        if (!item)
+            throw 'item não pode ser nulo ou indefinido'
+        if (!item.title || !item.title.trim())
+            throw 'item.title não pode ser nulo ou indefinido'
         this.items.push(item);
+        await this.saveListToDisk();
     }
 
-    removeItem(index: number) {
+    /**
+     * Remove um item da lista
+     */
+    async removeItem(index: number) {
         this.items.splice(index, 1);
+        await this.saveListToDisk();
     }
 
     getItems(): Item[] {
-        return this.items
+        return [...this.items];
     }
+
 }
 
+export default TodoList
+export { Item, TodoList }
